9928b07ce00f908a6e37287f166b81231391c3cc
janarth10 <janarthp10@gmail.com>
2021-04-25 16:28:19 -0400

    init

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ import requests
 import bs4
 from markdownify import markdownify
 
-OBSIDIAN_DIR = '/Users/newdev/Documents/janarth-vault/crawler'
+OBSIDIAN_DIR = '/Users/newdev/Documents/Folders/obsidian-vaults/hive-blog-vault'
 LOG_FILE = '/Users/newdev/Hive/Development/personal_projects/obsidian-crawler/error-log.txt'
 
 
@@ -17,6 +17,10 @@ def write_to_logs(message=''):
 
 
 def download_hive_blog_post_to_md_file(url):
+    if 'blog.hive.co' not in url:
+        write_to_logs(message=f"Tried to download {url}. Does not belong to Hive Blog")
+        return
+
     request = requests.get(url)
     request.raise_for_status()
 
@@ -32,11 +36,45 @@ def download_hive_blog_post_to_md_file(url):
          will ignore downloading again
         """
         with open(file_path, 'x') as f:
+            f.write(f"[Original Link]({url})")
+            f.write("\n\n")
             blog_content_div = soup.find("div", "post-content__inner")
             for html_tag in blog_content_div:
-                f.write(markdownify(str(html_tag), heading_style="ATX"))
+                if type(html_tag) == bs4.element.NavigableString:
+                    f.write(markdownify(str(html_tag), heading_style="ATX"))
+                elif html_tag.name == 'a' and 'blog.hive.co' in html_tag.get('href'):
+                    write_to_logs(message=f"individual a_tag: {html_tag.get('href')}")
+                    f.write(f"[[{download_hive_blog_post_to_md_file(url=html_tag.get('href'))}]]")
+                    f.write("\n")
+                else:
+                    for a_tag in html_tag.select('a'):
+                        write_to_logs(message=f"nested a_tag: {a_tag.get('href')}")
+                        if a_tag.name == 'a' and 'blog.hive.co' in a_tag.get('href'):
+                            f.write(f"[[{download_hive_blog_post_to_md_file(url=a_tag.get('href'))}]]")
+                            f.write("\n")
+                    f.write(markdownify(str(html_tag), heading_style="ATX"))
     except FileExistsError as e:
         write_to_logs(message=f"Tried to download {url}. Already exists.")
 
+    return file_name
+
 
 download_hive_blog_post_to_md_file(url='https://blog.hive.co/50-ecommerce-subject-lines-to-drive-higher-revenue-this-easter-long-weekend/')
+
+
+# def bfs_crawl():
+#     """
+#         Using breadth first search to download all blog posts in the Hive Blog "graph"
+
+#         TODO
+#         - where to start? Is there a way to get a list of all articles? If so don't need to BFS
+#         - need to replace "markdownify(str(a_tag), heading_style="ATX")" with [[<filename>]]. so that my
+#             obsidian files link to eachother
+#     """
+
+#     # for fucks just want to pseudo the BFS algo
+
+#     urls_queue = []
+#     for post_url in urls_queue:
+#         # define this fn within 'bfs_crawl' so that it can add new urls to que
+#         download_hive_blog_post_to_md_file(post_url)
