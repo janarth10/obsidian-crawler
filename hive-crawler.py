f7daea00aaa4b754273d0398692b3b9878dca9f5
janarth10 <janarthp10@gmail.com>
2021-04-29 09:48:36 -0400

    able to download majority of Hive blogs

--- hive-crawler.py
+++ hive-crawler.py
@@ -4,7 +4,7 @@ import requests
 import bs4
 from markdownify import markdownify
 
-OBSIDIAN_DIR = '/Users/newdev/Documents/Folders/obsidian-vaults/hive-blog-vault'
+OBSIDIAN_DIR = '/Users/newdev/Google Drive/Folders/obsidian-vaults/hive-blog-vault'
 LOG_FILE = '/Users/newdev/Hive/Development/personal_projects/obsidian-crawler/error-log.txt'
 
 
@@ -22,11 +22,11 @@ def download_hive_blog_post_to_md_file(url):
         return
 
     request = requests.get(url)
-	try:
-	    request.raise_for_status()
-	except Exception as e:
-		write_to_logs(message=e.message)
-		return
+    try:
+        request.raise_for_status()
+    except requests.HTTPError as e:
+        write_to_logs(message=str(e))
+        return
 
     soup = bs4.BeautifulSoup(request.text, "html.parser")
     file_name = f"{soup.find('title').text}.md"
@@ -63,38 +63,43 @@ def download_hive_blog_post_to_md_file(url):
     return file_name
 
 
-def download_hive_blogs_from_card_list_view(reponse):
-	soup = bs4.BeautifulSoup(response.text, 'html.parser')
-	
-	"""
-	    sanity check
-	        - soup.select('a.js-fadein')
-	        - soup.select('h2.post-card__title')
-	    returned 9 results when on first page. assuming that this will find all blog posts
-	"""
-	links = soup.select('a.js-fadein') or soup.select('h2.post-card__title')
-	for link in links:
-	    link_str = link.get('href')
-	    if 'blog.hive.co' not in link.get('href'):
-	        link_str = f"https://blog.hive.co{link_str}"
-	    download_hive_blog_post_to_md_file(url=link_str)
-	
-# __main__	
+def download_hive_blogs_from_card_list_view(response):
+    soup = bs4.BeautifulSoup(response.text, 'html.parser')
+
+    """
+        sanity check
+            - soup.select('a.js-fadein')
+            - soup.select('h2.post-card__title')
+        returned 9 results when on first page. assuming that this will find all blog posts
+    """
+    links = soup.select('a.js-fadein') or soup.select('h2.post-card__title')
+    for link in links:
+        link_str = link.get('href')
+        if 'blog.hive.co' not in link.get('href'):
+            link_str = f"https://blog.hive.co{link_str}"
+        download_hive_blog_post_to_md_file(url=link_str)
+
+# __main__
 start_urls = [
-	'https://blog.hive.co/tag/email-design/',
-	'https://blog.hive.co/tag/email-marketing/',
-	'https://blog.hive.co/tag/ecommerce-email/',
-	'https://blog.hive.co/tag/seasonal/',
+    'https://blog.hive.co/tag/email-design',
+    'https://blog.hive.co/tag/email-marketing',
+    'https://blog.hive.co/tag/ecommerce-email',
+    'https://blog.hive.co/tag/seasonal',
 ]
 
-page_num = 0
-while page_num < 15:
-	resp = requests.get(f"{start_urls[0]}page/{page_num}")
-	try:
-		resp.raise_for_status()
-	except Exception as e:
-		write_to_logs(message='Did we get to end of pages??')
-		write_to_logs(message=e.message)
-		break
-	download_hive_blogs_from_card_list_view(reponse=resp)
-	
+for start_url in start_urls:
+    page_num = 1
+    while page_num < 20:
+        resp = requests.get(f"{start_url}/page/{page_num}")
+        try:
+            resp.raise_for_status()
+            download_hive_blogs_from_card_list_view(response=resp)
+        except requests.HTTPError as e:
+            write_to_logs(message='Did we get to end of pages??')
+            write_to_logs(message=str(e))
+            break
+        except Exception as e:
+            write_to_logs(message='Unknown Exception')
+            write_to_logs(message=str(e))
+
+        page_num += 1
